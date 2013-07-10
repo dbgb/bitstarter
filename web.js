808d5000ef6c06c2c6fc68585210d67cf476e1e6
djb <dbayliss.mail@gmail.com>
2013-07-10 23:32:53 +0100

    Added index.html and updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf = fs.readFileSync('./index.html').toString();
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
