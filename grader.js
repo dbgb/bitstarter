cc20dd37b4f6350b43a8003f70240ee1f323e60d
djb <dbayliss.mail@gmail.com>
2013-07-12 01:55:14 +0100

    Fixed npm extraneous package warnings
    Preliminary updates to checks and grader files.

--- grader.js
+++ grader.js
@@ -25,8 +25,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.github.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -56,6 +58,11 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkURL = function(){
+    // Code goes here. --TODO
+
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -66,7 +73,8 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+        .option('-u, --url <check_url>', 'Path to URL', clone(checkURL), URL_DEFAULT)
+ 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
