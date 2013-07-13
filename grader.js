c8b5d223ef199b886607eab522eb3295b629767b
djb <dbayliss.mail@gmail.com>
2013-07-13 20:39:51 +0100

    Updated grader.js

--- grader.js
+++ grader.js
@@ -16,6 +16,9 @@ References:
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
+ + restler
+   - https://github.com/danwrong/Restler/
+
  + JSON
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
@@ -33,8 +36,8 @@ var URL_DEFAULT = "http://www.github.com";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -52,15 +55,20 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
 
-var checkURL = function(){
-    // Code goes here. --TODO
-
+var checkURL = function(url) {
+   restler.get(url).on('complete', function(result) {
+    if (result instanceof Error) {
+     console.log('Error: ' + result.message);
+    } else {
+     console.log(result);
+   }});
+   return;
 };
 
 var clone = function(fn) {
@@ -71,10 +79,10 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <check_url>', 'Path to URL', clone(checkURL), URL_DEFAULT)
- 	.parse(process.argv);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <check_url>', 'Path to URL', clone(checkURL), URL_DEFAULT)
+	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
